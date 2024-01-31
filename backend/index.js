b82c8c2f82f0bdf3b17bacd1ead4a6225d83153f
chandan9pandey <chandan9pandey@gmail.com>
2024-01-31 16:02:50 +0530

    database connection + express server created

--- backend/index.js
+++ backend/index.js
@@ -1,4 +1,3 @@
-const port = 4000;
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
@@ -6,6 +5,27 @@ const jwt = require("jsonwebtoken");
 const multer = require("multer");
 const path = require("path");
 const cors = require("cors");
+const dotenv = require("dotenv");
 
 app.use(express.json());
 app.use(cors());
+dotenv.config();
+
+//Database connection with MongoDB
+mongoose.connect(process.env.DB_URL);
+
+//API creation
+
+app.get("/", (req, res) => {
+	res.send("Express App is Running");
+});
+
+app.listen(process.env.PORT, (error) => {
+	{
+		if (!error) {
+			console.log("Server listening on port " + process.env.PORT);
+		} else {
+			console.log("Error: " + error);
+		}
+	}
+});
