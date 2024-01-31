cbaed2c64a93ab85cb209a0eeb6b15d542252436
chandan9pandey <chandan9pandey@gmail.com>
2024-01-31 18:31:34 +0530

    addproduct endpoint added

--- backend/index.js
+++ backend/index.js
@@ -46,6 +46,26 @@ app.post("/upload", upload.single("product"), (req, res) => {
 	});
 });
 
+// to add product
+
+app.post("/addproduct", async (req, res) => {
+	const product = new Product({
+		id: req.body.id,
+		name: req.body.name,
+		image: req.body.image,
+		category: req.body.category,
+		new_price: req.body.new_price,
+		old_price: req.body.old_price,
+	});
+	console.log(product);
+	await product.save();
+	console.log("Saved");
+	res.json({
+		success: true,
+		name: req.body.name,
+	});
+});
+
 app.listen(process.env.PORT, (error) => {
 	{
 		if (!error) {
