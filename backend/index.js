18939e054cdcc7fbcc99ea5c8dde088ea443120f
chandan9pandey <chandan9pandey@gmail.com>
2024-01-31 18:01:03 +0530

    created an image storage engine using multer

--- backend/index.js
+++ backend/index.js
@@ -20,6 +20,30 @@ app.get("/", (req, res) => {
 	res.send("Express App is Running");
 });
 
+// Image storage engine
+
+const storage = multer.diskStorage({
+	destination: "./upload/images",
+	filename: (req, file, cb) => {
+		return cb(
+			null,
+			`${file.fieldname}_${Date.now()}${path.extname(file.originalname)}`
+		);
+	},
+});
+
+const upload = multer({ storage: storage });
+
+//creating upload endpoint for images
+
+app.use("./images", express.static("upload/images"));
+app.post("/upload", upload.single("product"), (req, res) => {
+	res.json({
+		success: true,
+		image_url: `http://localhost:${process.env.PORT}/images/${req.file.filename}`,
+	});
+});
+
 app.listen(process.env.PORT, (error) => {
 	{
 		if (!error) {
