3cc33e45f71ebc8155bda9680901882cfe78fa15
chandan9pandey <chandan9pandey@gmail.com>
2024-02-02 01:03:20 +0530

    Login Sign Up page toggle

--- frontend/src/Pages/LoginSignup.jsx
+++ frontend/src/Pages/LoginSignup.jsx
@@ -1,23 +1,60 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./CSS/LoginSignup.css";
 const LoginSignup = () => {
+	const [state, setState] = useState("Log In");
+	const [containerHeight, setContainerHeight] = useState(400);
+	const [loginSignupHeight, setLoginSignupHeight] = useState(100);
+
+	useEffect(() => {
+		if (state === "Sign Up") {
+			setContainerHeight(600);
+			setLoginSignupHeight(120);
+		} else {
+			setContainerHeight(400);
+			setLoginSignupHeight(100);
+		}
+	}, [state]);
+
 	return (
-		<div className="loginsignup">
-			<div className="loginsignup-container">
-				<h1>Sign Up</h1>
+		<div className="loginsignup" style={{ height: `${loginSignupHeight}vh` }}>
+			<div
+				className="loginsignup-container"
+				style={{ height: `${containerHeight}px` }}
+			>
+				<h1>{state}</h1>
 				<div className="loginsinup-fields">
-					<input type="text" placeholder="Your Name" />
+					{state === "Sign Up" ? (
+						<input type="text" placeholder="Your Name" />
+					) : (
+						""
+					)}
 					<input type="email" placeholder="Email Address" />
 					<input type="password" placeholder="Password" />
 				</div>
-				<div className="loginsignup-agree">
-					<input type="checkbox" name="" id="" />
-					<p>By continuing , I agree to the terms of use & privacy policy</p>
-				</div>
-				<button>Continue</button>
-				<p className="loginsignup-login">
-					Already have an account? <span>Login here</span>
-				</p>
+				{state === "Sign Up" ? (
+					<>
+						<div className="loginsignup-agree">
+							<input type="checkbox" name="" id="" />
+							<p>
+								By continuing , I agree to the terms of use & privacy policy
+							</p>
+						</div>
+						<button>Continue</button>
+					</>
+				) : (
+					<button>Log In</button>
+				)}
+				{state === "Sign Up" ? (
+					<p className="loginsignup-login">
+						Already have an account?{" "}
+						<span onClick={() => setState("Log In")}>Log In</span>
+					</p>
+				) : (
+					<p className="loginsignup-login">
+						Start by creating an account.{" "}
+						<span onClick={() => setState("Sign Up")}>Click here</span>
+					</p>
+				)}
 			</div>
 		</div>
 	);
