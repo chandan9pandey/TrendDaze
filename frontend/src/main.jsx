75fa3d17b7975d1300d72e2b5d6d0f91af72d4da
chandan9pandey <chandan9pandey@gmail.com>
2024-02-11 00:11:29 +0530

    + React Strictmode

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,11 +1,13 @@
-import React from "react";
+import React, { StrictMode } from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 import ShopContextProvider from "./Context/ShopContext.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-	<ShopContextProvider>
-		<App />
-	</ShopContextProvider>
+	<StrictMode>
+		<ShopContextProvider>
+			<App />
+		</ShopContextProvider>
+	</StrictMode>
 );
